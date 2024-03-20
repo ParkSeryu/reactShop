43cd9c08bbac4afc3e04a00ec00cac106d671b08
parkseryu <pjh971212@gmail.com>
2024-03-20 20:18:31 +0900

    3. 코드 길어지면 import export 하면 됩니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {Button, Container, Nav, Navbar} from "react-bootstrap";
+import {Container, Nav, Navbar} from "react-bootstrap";
 import bg from './img/bg.png';
 import shoes1 from './img/shoes1.jpg';
+import 서버 from './data'
 
 function App() {
+    let [shoes] = useState(서버);
     return (
         <div className="App">
+
             <Navbar bg="dark" data-bs-theme="dark">
                 <Container>
                     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
@@ -25,19 +28,21 @@ function App() {
                 <div className="row">
                     <div className="col-md-4">
                         <img src={shoes1} width={"80%"}></img>
-                        <h4>상품명</h4>
-                        <p>상품설명</p>
+                        <h4>{shoes[0].title}</h4>
+                        <p>{shoes[0].content}</p>
                     </div>
                     <div className="col-md-4"><img src={"https://codingapple1.github.io/shop/shoes2.jpg"}
                                                    width={"80%"}></img>
-                        <h4>상품명</h4>
-                        <p>상품설명</p></div>
+                        <h4>{shoes[1].title}</h4>
+                        <p>{shoes[1].content}</p>
+                    </div>
                     <div className="col-md-4">
-                        <img src={"/logo512.png"}
+                        <img src={"https://codingapple1.github.io/shop/shoes3.jpg"}
                              alt={"shoes3"}
                              width={"80%"}></img>
-                        <h4>상품명</h4>
-                        <p>상품설명</p></div>
+                        <h4>{shoes[2].title}</h4>
+                        <p>{shoes[2].content}</p>
+                    </div>
                 </div>
             </div>
         </div>
