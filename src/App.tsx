4281ae554477bc453e2f90d316f6aba363508db9
parkseryu <pjh971212@gmail.com>
2024-03-24 16:02:15 +0900

    5. 리액트 라우터 : 1. 셋팅이랑 기본 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {Container, Nav, Navbar} from "react-bootstrap";
 import bg from './img/bg.png';
-import shoes1 from './img/shoes1.jpg';
-import 서버 from './data'
+import data from './data'
 import ShoesComponent from "./ShoesComponent";
+import {Routes, Route, Link} from 'react-router-dom';
+import DetailComponent from "./DetailComponent";
 
 
 function App() {
-    let [shoes] = useState(서버);
+    let [shoes] = useState(data);
     return (
         <div className="App">
 
@@ -17,23 +18,30 @@ function App() {
                 <Container>
                     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Link to="/">홈ㅁㄴㅇㄹ </Link>
+                        <Link to="/detail"> 상세페이지</Link>
                     </Nav>
 
                 </Container>
             </Navbar>
-            <div className={"main-bg"} style={{backgroundImage: 'url(' + bg + ')'}}></div>
 
-            <div className="container">
-                <div className="row">
-                    {shoes.map((ele, index) => {
-                        return <ShoesComponent key={index} id={ele.id} title={ele.title} content={ele.content}
-                                               price={ele.price}/>
-                    })}
-                </div>
-            </div>
+
+            <Routes>
+                <Route path={"/"} element={<>
+                    <div className={"main-bg"} style={{backgroundImage: 'url(' + bg + ')'}}></div>
+
+                    <div className="container">
+                        <div className="row">
+                            {shoes.map((ele, index) => {
+                                return <ShoesComponent key={index} id={ele.id} title={ele.title} content={ele.content}
+                                                       price={ele.price}/>
+                            })}
+                        </div>
+                    </div>
+                </>}/>
+                <Route path="/detail" element={<DetailComponent/>}/>
+            </Routes>
+
         </div>
     );
 }
