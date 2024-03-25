dc1e5ab1b4e2e59bc65fd9a5f70d2fe0dcb05195
parkSeryu <pjh971212@gmail.com>
2024-03-25 21:32:46 +0900

    10. Lifecycle과 useEffect 2

--- src/routes/DetailComponent.tsx
+++ src/routes/DetailComponent.tsx
@@ -21,6 +21,7 @@ let Box = styled.div`
 const DetailComponent = (props: any) => {
     let [count, setCount] = useState(0);
     const [show, setShow] = useState(true);
+    const [text, setText] = useState<any>();
     const {id} = useParams();
     const index = Number(id)
     const test = props.shoes.find((ele: {
@@ -28,10 +29,24 @@ const DetailComponent = (props: any) => {
     }) => ele.id === index)
 
     useEffect(() => {
+        console.log(2)
+
         setTimeout(() => {
             setShow(false);
         }, 1000)
-    },);
+
+        return () => {
+            console.log(1)
+        }
+    }, []);
+
+    useEffect(() => {
+
+
+        if (Number.isNaN(text)) {
+            alert('그러지마세요');
+        }
+    }, [text]);
 
     // @ts-ignore
     return (
@@ -51,7 +66,7 @@ const DetailComponent = (props: any) => {
                     <img src={"https://codingapple1.github.io/shop/shoes1.jpg"} width="100%"/>
                 </div>
                 <div className={"col-md-6"}>
-
+                    <input onChange={(e) => setText(e.target.value)}></input>
                     <h4 className={"pt-5"}>{test.title}</h4>
                     <p>{test.content}</p>
                     <p>{test.price}</p>
